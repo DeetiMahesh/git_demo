e953393e6ece4354999e55e3cc6dfd1ef26b2cb9
DeetiMahesh <128590473+DeetiMahesh@users.noreply.github.com>
2025-11-16 18:13:26 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -7,11 +7,12 @@ sudo yum install jenkins git -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-sudo mkdir -p /var/tmp_disk
+sudo mkdir -p /var/tmp_disksdsafasfa
 sudo chmod 1777 /var/tmp_disk
 sudo mount --bind /var/tmp_disk /tmp
 echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
 sudo systemctl mask tmp.mount
 df -h /tmp
 sudo systemctl restart jenkins
+
 
